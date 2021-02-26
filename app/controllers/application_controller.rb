ba8a9693de684f9afa89abbc5e671ab587ddb319
fitzgebt <fitzgebt@gmail.com>
2021-02-26 15:26:32 -0800

    commit tree 8f94689afab845eba352259c58213196db6d01a1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+
+    @articles = Article.all
+    erb :index
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{Article.last.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect to "/articles/#{params[:id]}"
+  end
+
+  delete '/articles/:id' do
+    Article.find(params[:id]).destroy
+    redirect to "/articles"
+  end
+  
 end
